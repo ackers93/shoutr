c7671fb3d61bfad559da6710e6f49a7044ac78df
Andrew Ackerman <andrew.ackers@gmail.com>
2019-04-11 10:31:19 -0700

    like and unlike button functionality

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -2,4 +2,12 @@ module ShoutHelper
   def avatar(user)
   image_tag "https://picsum.photos/60/60/?random(shout.user.email)}"
 end
+
+  def like_button(shout)
+    if current_user.liked?(shout)
+      link_to "Unlike", unlike_shout_path(shout), method: :delete
+    else
+      link_to "Like", like_shout_path(shout), method: :post
+    end
+  end
 end
